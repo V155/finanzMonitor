0ad00f1f4dc0dc68c07dd856982d9cfef5f20cf5
Fritz Reichwald <fritz.reichwald@gmail.com>
2014-02-23 15:43:19 +0100

    created month struct that holds all data

--- dataFunctions.h
+++ dataFunctions.h
@@ -21,6 +21,22 @@ struct fixcost
 	int price;
 };
 
+struct month
+{
+	int month;
+	int saldoLastMonth;
+	struct entry entries[100];
+	struct fixcost incomes[50];
+	struct fixcost bills[50];
+	int entriesIndex;
+	int incomesIndex;
+	int billsIndex;
+	int sum1;
+	int sums[16];
+	char knCats[16][8];
+	int numCat;
+};
+
 
 void createEntry(int* entI, struct entry* entries, char category[8], char desc[16], int price, int date);
 int readIn(int* incI, struct fixcost* incomes, int* bilI, struct fixcost* bills, int* entI, struct entry* entries);
